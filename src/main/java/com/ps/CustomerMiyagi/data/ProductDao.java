c3463931022676b16abcea4e67a1a860baf35afc
Fai Alateeq <fairouzalatteq@gmail.com>
2024-12-10 15:03:39 -0600

    ProductDao interface done

--- src/main/java/com/ps/CustomerMiyagi/data/ProductDao.java
+++ src/main/java/com/ps/CustomerMiyagi/data/ProductDao.java
@@ -1,4 +1,14 @@
 package com.ps.CustomerMiyagi.data;
 
+import com.ps.CustomerMiyagi.models.Customer;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 public interface ProductDao {
+    List<Customer> findAllProducts();
+    Customer findOneProduct(int id);
+    Customer createProduct(Customer customer);
+    void updateProduct(int id, Customer customer);
+    void deleteAProduct(@PathVariable int id);
 }
