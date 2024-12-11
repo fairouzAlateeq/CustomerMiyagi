477a1b5229c6d4d012b34ea8b767d30089cdca29
Fai Alateeq <fairouzalatteq@gmail.com>
2024-12-11 14:51:07 -0600

    finished product methods

--- src/main/java/com/ps/CustomerMiyagi/data/mysql/MySqlProductDao.java
+++ src/main/java/com/ps/CustomerMiyagi/data/mysql/MySqlProductDao.java
@@ -96,12 +96,34 @@ public class MySqlProductDao implements ProductDao {
 
     @Override
     public void updateProduct(int id, Product product) {
+        String query = "update product set name = ?, phone = ?, category_id = ? where product_id = ?";
+        try(
+                Connection connection = this.dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ) {
+            preparedStatement.setString(1, product.getName());
+            preparedStatement.setFloat(2, product.getPrice());
+            preparedStatement.setString(3, product.getCategoryId());
+            preparedStatement.setInt(3, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void deleteAProduct(int id) {
-
+        String query = "delete from product where product_id = ?";
+        try(
+                Connection connection = this.dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ) {
+            preparedStatement.setInt(1, id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private Product mapProduct(ResultSet resultSet) throws SQLException {
