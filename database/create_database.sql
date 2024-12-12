7bca211cd610eca81d72a2299d9285ed8e944096
Fai Alateeq <fairouzalatteq@gmail.com>
2024-12-12 09:19:45 -0600

    fixes

--- database/create_database.sql
+++ database/create_database.sql
@@ -21,6 +21,33 @@ INSERT INTO `customer` (`customer_id`, `name`, `phone`) VALUES
 (9,'Isabella', '444444444'),
 (10,'Noah', '111111111');
 
+create table category (
+`category_id` INTEGER NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+CONSTRAINT `category` PRIMARY KEY (`category_id`)
+);
+
+INSERT INTO `category` (`category_id`,`name`) VALUES
+(1,'Honey Products'),
+(2, 'Beekeeping Equipment'),
+(3, 'Supplements'),
+(4, 'Candles'),
+(5, 'Cosmetics'),
+(6, 'Edible Items'),
+(7, 'Frames'),
+(8, 'Accessories'),
+(9, 'Toys'),
+(10,'Gift Sets'),
+(11,'Raw Honey'),
+(12,'Organic Products'),
+(13,'Flavored Honey'),
+(14,'Syrups'),
+(15,'Tools'),
+(16,'Books'),
+(17,'Beeswax'),
+(18,'Propolis'),
+(19,'Royal Jelly');
+
 create table product (
 `product_id` INTEGER NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(50) NOT NULL,
@@ -30,42 +57,16 @@ CONSTRAINT `pk_customer` PRIMARY KEY (`product_id`),
 CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES category(`category_id`)
 );
 
-create table category (
-`category_id` INTEGER NOT NULL AUTO_INCREMENT,
-`name` VARCHAR(50) NOT NULL,
-CONSTRAINT `category` PRIMARY KEY (`category_id`)
-);
-
-INSERT INTO category (`name`) VALUES
-('Honey Products'),
-('Beekeeping Equipment'),
-('Supplements'),
-('Candles'),
-('Cosmetics'),
-('Edible Items'),
-('Frames'),
-('Accessories'),
-('Toys'),
-('Gift Sets'),
-('Raw Honey'),
-('Organic Products'),
-('Flavored Honey'),
-('Syrups'),
-('Tools'),
-('Books'),
-('Beeswax'),
-('Propolis'),
-('Royal Jelly');
 
 -- Insert rows into the 'product' table
-INSERT INTO product (`name`, `price`, `category_id`) VALUES
-('Honey Jar', 9.99, 1),
-('Honey Mustard', 7.99, 13),
-('Propolis Soap', 2.99, 18),
-('Lip Balm', 3.49, 5),
-('Bee Wax Candle', 11.99, 4),
-('Royal Jelly Supplement', 14.99, 19),
-('Bee Pollen Granules', 14.99, 3),
-('Honey Candy', 3.99, 6),
-('Honeycomb Frame', 45.00, 7),
-('Beekeeping Suit', 255.00, 2);
+INSERT INTO `product` (`product_id`,`name`, `price`, `category_id`) VALUES
+(1,'Honey Jar', 9.99, 1),
+(2,'Honey Mustard', 7.99, 13),
+(3,'Propolis Soap', 2.99, 18),
+(4, 'Lip Balm', 3.49, 5),
+(5, 'Bee Wax Candle', 11.99, 4),
+(6, 'Royal Jelly Supplement', 14.99, 19),
+(7, 'Bee Pollen Granules', 14.99, 3),
+(8, 'Honey Candy', 3.99, 6),
+(9, 'Honeycomb Frame', 45.00, 7),
+(10, 'Beekeeping Suit', 255.00, 2);
